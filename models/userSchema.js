594564ca1f32852cfda6e094254979c4a3723bbb
Akash <akashmeghwal0007@gmail.com>
2024-08-20 23:25:39 +0530

    fif

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
+const bcrypt = require("bcryptjs");
 import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
